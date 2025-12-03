ade0245e0bfaca1b69edfa65b435ef2bc498e19f
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2025-12-02 20:23:31 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header({ currentUser, users = [], onProfileClick }) {
           <Trophy size={28} />
         </div>
         <div className="title-wrap">
-          <h1 className="app-title">V. Score</h1>
+          <h1 className="app-title">GlobalScore</h1>
           <div className="app-sub">Inicio</div>
         </div>
       </div>
