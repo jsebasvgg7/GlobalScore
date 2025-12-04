9533a56e9e159a27ea10ff61792162e84ad6aaa3
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2025-12-04 15:46:40 -0500

    v47

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -430,7 +430,7 @@ export default function StatsPage({ currentUser }) {
                   </div>
                   <div className="league-accuracy">
                     <div className="accuracy-circle">
-                      <svg width="60" height="60">
+                      <svg viewBox="0 0 60 60" preserveAspectRatio="xMidYMid meet">
                         <circle
                           cx="30"
                           cy="30"
