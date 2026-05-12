72ee6d69595f63ac101e0c47dc96b5c8f5393278
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2026-05-12 15:05:11 -0500

    v27

--- src/shared/ui/index.js
+++ src/shared/ui/index.js
@@ -1,4 +1,13 @@
 export { default as GlobalLoader } from './GlobalLoader'
+
+export {
+    PageLoader,
+    LoadingOverlay,
+    Spinner,
+    ButtonLoader,
+    LoadingDots,
+} from './GlobalLoader'
+
 export { default as ImageViewer } from './ImageViewer'
 export { default as LoadingSpinner } from './LoadingSpinner'
 export { default as LoadingStates } from './LoadingStates'
